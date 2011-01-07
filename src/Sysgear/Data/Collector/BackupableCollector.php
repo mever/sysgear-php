55491d6516bd8913a0d50143252d1025dd85c2d4
Martijn Evers <martijn4evers@gmail.com>
2011-01-07 23:38:05 +0100

    Split Object and Backupable collector

---
+++ src/Sysgear/Data/Collector/BackupableCollector.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Sysgear\Data\Collector;
+
+use Sysgear\Backup\BackupableInterface;
+
+class BackupableCollector extends ObjectCollector
+{
+    protected function scanCompositeProperty($object, $name, $value)
+    {
+        // Scan BackupableInterface implmentation
+        if ($value instanceof BackupableInterface) {
+            $this->addBackupableChild($object, $value);
+        }
+
+        // Scan array and array like.
+        if ($this->recursiveScan
+        && (is_array($value) || ($value instanceof \IteratorAggregate))) {
+
+            $collection = $this->document->createElement($name);
+            $this->element->appendChild($collection);
+
+            foreach ($value as $elem) {
+
+                // Collect array element objects implementing the BackupableInterface.
+                if ($elem instanceof BackupableInterface) {
+                    $this->addBackupableChild($object, $elem, $collection);
+                }
+            }
+        }
+    }
+
+    /**
+     * Add child node to this collection.
+     * 
+     * @param \StdClass $parentObj
+     * @param \Sysgear\Backup\BackupableInterface $backupable
+     * @param \DOMNode $node
+     */
+    protected function addBackupableChild($parentObj, $backupable, \DOMNode $node = null)
+    {
+        // Make a copy of this collector to allow recursive collecting.
+        $collector = clone $this;
+        $collector->excludedObjects[] = $parentObj;
+        if (in_array($backupable, $this->excludedObjects, true)) {
+            $collector->recursiveScan = false;
+        }
+        $backupable->backup($collector);
+        
+        if (null === $node) {
+            $node = $this->element;
+        }
+        $node->appendChild($collector->getDomElement());
+    }
+}
--- src/Sysgear/Data/Collector/ObjectCollector.php
+++ src/Sysgear/Data/Collector/ObjectCollector.php
@@ -45,34 +45,27 @@ class ObjectCollector extends AbstractCollector
                 $name = $property->getName();
                 $value = $property->getValue($object);
 
-                // Scan scalars.
+                // Scan scalar.
                 if (is_scalar($value)) {
                     $this->element->setAttribute($name, $value);
-                }
-
-                // Scan array and array like.
-                if ($this->recursiveScan
-                && (is_array($value) || ($value instanceof \IteratorAggregate))) {
-
-                    $collection = $this->document->createElement($name);
-                    $this->element->appendChild($collection);
-
-                    foreach ($value as $arrayElem) {
-
-                        // Collect array element objects implementing the BackupableInterface.
-                        if (($arrayElem instanceof BackupableInterface) && (! in_array($arrayElem, $this->excludedObjects, true))) {
-
-                            // Make a copy of this collector to allow recursive collecting.
-                            $collector = clone $this;
-                            $collector->excludedObjects[] = $object;
-                            $arrayElem->backup($collector);
-                            $collection->appendChild($collector->getDomElement());
-                        }
-                    }
+                } else {
+                    $this->scanCompositeProperty($object, $name, $value);
                 }
             }
         }
         $this->document->appendChild($this->element);
+    }
+
+    /**
+     * 
+     * 
+     * @param unknown_type $object
+     * @param unknown_type $name
+     * @param unknown_type $value
+     */
+    protected function scanCompositeProperty($object, $name, $value)
+    {
+        // TODO: Scan none-scalar values.
     }
 
     /**
