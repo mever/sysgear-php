2c2538d93abfcf22dec4ba59107bce7622289896
Martijn Evers <martijn4evers@gmail.com>
2011-02-22 21:28:28 +0100

    New feature: return operator as SQL comparison operator

--- src/Sysgear/Operator.php
+++ src/Sysgear/Operator.php
@@ -13,4 +13,19 @@ class Operator
     const NUM_LESS_OR_EQUAL_THAN = 6;
     const STR_START_WITH = 7;
     const STR_END_WITH = 8;
+
+    public static function toSqlComparison($operator)
+    {
+        switch ($operator) {
+        case self::LIKE: return 'LIKE';
+        case self::EQUAL: return '=';
+        case self::NOT_EQUAL: return '<>';
+        case self::NUM_GREATER_OR_EQUAL_THAN: return '>=';
+        case self::NUM_GREATER_THAN: return '>';
+        case self::NUM_LESS_OR_EQUAL_THAN: return '<=';
+        case self::NUM_LESS_THAN: return '<';
+        case self::STR_END_WITH: return 'LIKE';
+        case self::STR_START_WITH: return 'LIKE';
+        }
+    }
 }
